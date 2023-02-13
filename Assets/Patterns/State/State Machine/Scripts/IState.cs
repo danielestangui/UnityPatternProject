f73fb99d8ce42ef8b32f5de6955c3d8a7fa4fe17
Daniel Estangüi <daniel.estangui@xoia.es>
2023-02-13 09:25:16 +0100

    Add a comment with a reference on a Fluent State Machine

--- Assets/Patterns/State/State Machine/Scripts/IState.cs
+++ Assets/Patterns/State/State Machine/Scripts/IState.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Fluent State Machine reference: https://github.com/Real-Serious-Games/Fluent-State-Machine
 namespace Patterns.State.StateMachine 
 {
     public interface IState
